c708b3d4214de312aff66b5f39f09582cccb29f8
chenbinwu <chenbinwu66@gmail.com>
2025-03-06 00:26:54 -0500

    push

--- mp01.R
+++ mp01.R
@@ -1,3 +1,16 @@
+#!/usr/bin/env Rscript
+if(!require("quarto")){
+  install.packages("quarto")
+}
+library(quarto)
+if(!quarto::quarto_binary_sitrep()){
+  stop("Something is wrong with your quarto installation.")
+}
+quarto::quarto_render(".")
+system("git add docs/*")
+if(!any(grepl("rstudio", search()))){q("no")}
+
+
 if(!file.exists("data/mp01/nyc_payroll_export.csv")){
   dir.create("data/mp01", showWarnings=FALSE, recursive=TRUE)
   
